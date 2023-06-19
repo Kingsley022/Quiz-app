c12b3b9cd4a5f4ebb6328770851373c139e60243
Okereke Kingsley <kingsleyokereke911@gmail.com>
2023-06-19 14:04:42 +0100

    Added eniromental variables

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'font-awesome/css/font-awesome.css'; 
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools();
 
 
 
